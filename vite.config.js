ed0996fb0800f3c00a75e768e6d304b57726c1d1
Kellen <kellenringen@gmail.com>
2025-06-10 23:59:33 -0700

    Fix production build: font-awesome + vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // use project root
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html'), // <-- this tells Vite where to start
-    },
-  },
+  base: './', // ensures local asset paths work in production
 })
 
