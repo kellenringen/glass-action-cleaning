d87409197401848337fb03af80f9b2780ad12273
Kellen <kellenringen@gmail.com>
2025-08-18 12:54:34 -0700

    Fix: increased iframe height so submit button shows

--- src/App.jsx
+++ src/App.jsx
@@ -352,7 +352,7 @@ function App() {
           id="markate-widget-contact-iframe"
           src="https://www.markate.com/public/widget/contact?id=354173a034482a00258a54ec76edea93:36100:09b15aee"
           width="100%"
-          height="1100"
+          height="1200"
           scrolling="no"
           frameBorder="0"
           allowTransparency="true"
